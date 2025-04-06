5bc1ccb826390f088914ea8d9459efdd027a937d
Charles Siegel <charles.m.siegel@gmail.com>
2025-04-06 01:43:10 -0700

    improvement of rote and effect

--- characters/views/mage/rote.py
+++ characters/views/mage/rote.py
@@ -1,3 +1,6 @@
+from characters.models.core import Ability, Attribute
+from characters.models.mage.effect import Effect
+from characters.models.mage.focus import Practice
 from characters.models.mage.rote import Rote
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
@@ -23,3 +26,10 @@ class RoteListView(ListView):
     model = Rote
     ordering = ["name"]
     template_name = "characters/mage/rote/list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["practices"] = Practice.objects.all().order_by("name")
+        context["attributes"] = Attribute.objects.all().order_by("name")
+        context["abilities"] = Ability.objects.all().order_by("name")
+        return context
