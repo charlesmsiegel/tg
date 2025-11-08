44340b3605300b30fc220b32fff724e5ff1951d4
Charles Siegel <charles.m.siegel@gmail.com>
2025-11-08 11:00:43 -0800

    Update characters app index template   - Update characters/templates/characters/index.html to use tg-card components   - Replace inline styles with tg-nav-tabs component classes   - Apply standardized page header with gameline font support   - Update character list display to use tg-table or enhanced card layout   - Ensure status badges use tg-badge component classes

---
+++ characters/forms/core/group_creation.py
@@ -0,0 +1,29 @@
+from django import forms
+from game.models import ObjectType
+
+
+class GroupCreationForm(forms.Form):
+    group_type = forms.ChoiceField(choices=[])
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
+        super().__init__(*args, **kwargs)
+        
+        # Group types that should be available
+        group_types = ["cabal", "pack", "motley", "group"]
+        
+        if user and user.is_authenticated:
+            if user.profile.is_st():
+                # STs can create all group types
+                choices = [
+                    (x.name, x.name.replace("_", " ").title())
+                    for x in ObjectType.objects.filter(type="char", name__in=group_types)
+                ]
+                # Sort alphabetically by label
+                self.fields["group_type"].choices = sorted(choices, key=lambda x: x[1])
+            else:
+                # Regular users can only create cabals (mage groups)
+                self.fields["group_type"].choices = [
+                    (x.name, x.name.replace("_", " ").title())
+                    for x in ObjectType.objects.filter(type="char", name="cabal")
+                ]
