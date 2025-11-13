7d201ae318955b1a6438164220a82ad0252926f3
Charles Siegel <charles.m.siegel@gmail.com>
2025-11-13 01:39:38 -0800

    Update to game app templates

--- core/templatetags/field.py
+++ core/templatetags/field.py
@@ -9,3 +9,14 @@ def field(form, field_name):
         return form[field_name]
     except:
         return ""
+
+
+@register.filter(name="add_class")
+def add_class(field, css_class):
+    """
+    Add CSS class to a form field widget.
+    Usage: {{ form.field|add_class:"my-class" }}
+    """
+    if hasattr(field, 'as_widget'):
+        return field.as_widget(attrs={'class': css_class})
+    return field
