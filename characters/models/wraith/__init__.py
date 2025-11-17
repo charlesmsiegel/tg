aa33409e145bd3c495022f23aae2056ebef8d71b
Charles Siegel <charles.m.siegel@gmail.com>
2025-11-17 14:27:09 -0800

    Rename ThoronRating to ThornRating and update references accordingly

--- characters/models/wraith/__init__.py
+++ characters/models/wraith/__init__.py
@@ -5,5 +5,5 @@ from .guild import Guild
 from .passion import Passion
 from .shadow_archetype import ShadowArchetype
 from .thorn import Thorn
-from .wraith import Wraith, ThoronRating
+from .wraith import Wraith, ThornRating
 from .wtohuman import WtOHuman
